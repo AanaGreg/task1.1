ba47e4b609977757aaf60f754597bfb2b076eefd
AanaGreg <ana.gregic0@gmail.com>
2024-12-01 19:01:03 +0100

    Add files via upload

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val birthYear = 1995
+    val currentYear = 2024
+    val age1 = (currentYear - birthYear)
+    val name = ("Ana")
+    println("In 2024 $name is: $age1")
+    val year = 2048
+    val age2 = (year - birthYear)
+    println("In 2048 $name will be: $age2")
+}
